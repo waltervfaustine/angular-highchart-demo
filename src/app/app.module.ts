edef8b06147b718acf73ec462b55b085f5e07995
Walter V. Faustine <waltervfaustine@gmail.com>
2019-07-03 13:59:08 +0300

    chore: project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutputGraphComponent } from './output-graph/output-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputGraphComponent
   ],
   imports: [
     BrowserModule,
